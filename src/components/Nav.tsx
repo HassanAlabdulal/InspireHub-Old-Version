b430813378263ba93880589139adbc3a92944f6c
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-08 23:46:30 +0300

    Added some animations to the header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -14,14 +14,17 @@ const navMotion = {
     opacity: 0,
   },
 };
+
 const itemMotion = {
   visible: { opacity: 1, x: 0 },
   hidden: { opacity: 0, x: -100 },
 };
+
 const itemMotionDesktop = {
   visible: { opacity: 1, x: 0 },
   hidden: { opacity: 1, x: 0 },
 };
+
 const navLinks = [
   { name: "Home", href: "/", id: 1 },
   { name: "Catalog", href: "/Catalog", id: 2 },
@@ -42,6 +45,7 @@ const NavLinks = ({
         key={id}
         variants={isMobile ? itemMotion : itemMotionDesktop}
         href={href}
+        // className="nav-link relative inline-block text-black no-underline hover:underline"
       >
         {name}
       </motion.a>
@@ -53,25 +57,6 @@ export default function Nav() {
   const [toggled, setToggled] = useState(false);
   return (
     <nav className=" relative mx-8 mb-24 flex items-center justify-between pb-6 pt-12 font-medium md:mx-16 lg:mx-32">
-      {/* <svg
-        className="absolute bottom-0 left-1/2 -translate-x-1/2  "
-        width="250"
-        height="4"
-        viewBox="0 0 250 4"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.path
-          animate={{ pathLength: 1, opacity: 1 }}
-          initial={{ pathLength: 0, opacity: 0 }}
-          transition={{ delay: 1, duration: 0.75 }}
-          d="M2 2L428 1.99996"
-          stroke="#282828"
-          strokeWidth="2"
-          strokeLinecap="round"
-        />
-      </svg> */}
-
       <h1 className="text-xl font-bold tracking-wider transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ">
         <a href="/">InspireHub</a>
       </h1>
@@ -83,21 +68,50 @@ export default function Nav() {
           animate="visible"
           initial="hidden"
           className="fixed left-0 top-0  z-40 flex h-screen
-          w-full flex-col items-center  justify-center  gap-24 bg-white text-2xl font-bold text-center"
+          w-full flex-col items-center justify-center  gap-24 bg-white text-2xl font-bold text-center"
         >
           <NavLinks
             className=" flex flex-col gap-24 text-lg "
             isMobile={true}
           />
+          <motion.div
+            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 100 }}
+            transition={{ delay: 0.75 }}
+            className="flex flex-col gap-4 w-64"
+          >
+            <motion.a
+              className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 w-full text-white rounded transition-all 1s h-10"
+              href="/SignIn"
+            >
+              Sign in
+            </motion.a>
+            <motion.a
+              className="border-2 border-violet-500 hover:bg-violet-500 hover:text-white active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 w-full text-violet-500 rounded transition ease-in-out duration-750 h-10 tracking-wide"
+              href="/SignUp"
+            >
+              Sign up
+            </motion.a>
+          </motion.div>
         </motion.div>
       )}
       <motion.div
-        animate={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: 25 }}
+        animate={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -15 }}
         transition={{ delay: 0.35 }}
         className="hidden xl:flex xl:items-center  xl:justify-center xl:gap-12 xl:text-lg"
       >
         <NavLinks className="flex gap-12" isMobile={false} />
+      </motion.div>
+
+      <motion.div
+        animate={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -15 }}
+        transition={{ delay: 0.35 }}
+        className="flex gap-3 max-xl:text-white"
+      >
+        <a href="/SignIn">Sign in</a>
+        <a href="/SignUp">Sign up</a>
       </motion.div>
 
       {/* Hamburger Toggle */}
