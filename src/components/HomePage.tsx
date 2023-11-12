448d9c19297affbe3430028de2c6c1149456c324
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-12 09:45:41 +0300

    Add simple styles to main content

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>
+    <main>
+      <h1 className="flex items-center justify-center ml-8 w-full font-bold tracking-wide text-2xl">
         InspireHub: Where Ideas Take Flight and Creativity Meets Opportunity!
       </h1>
-    </div>
+    </main>
   );
 }
