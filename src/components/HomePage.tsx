83cd9040715c3dbaf479ceada6ce3a2fcf850e81
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-12 23:22:13 +0300

    Add get started button

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,3 @@
-import { motion } from "framer-motion";
-import { useState } from "react";
-
 export default function HomePage() {
   return (
     <main className="mt-56 w-full max-md:mt-36">
@@ -13,6 +10,10 @@ export default function HomePage() {
           Dive into a collaborative universe where your projects earn the
           spotlight and feedback fuels your next big leap
         </p>
+
+        <button className="bg-indigo-600 text-white py-2 px-6 mt-4 rounded hover:bg-indigo-500 duration-300">
+          Get Started
+        </button>
       </div>
     </main>
   );
