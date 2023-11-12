2f5f8964da3cce4b25219b63b119a5bdf4fee4d3
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-12 09:28:55 +0300

    Add HomePage component

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>
+        InspireHub: Where Ideas Take Flight and Creativity Meets Opportunity!
+      </h1>
+    </div>
+  );
+}
