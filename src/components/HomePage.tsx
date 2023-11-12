95f2c4eda0ade6ddd26ec2f5858dd9eb3745003d
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-12 23:41:27 +0300

    Add styles to get started button

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -11,8 +11,33 @@ export default function HomePage() {
           spotlight and feedback fuels your next big leap
         </p>
 
-        <button className="bg-indigo-600 text-white py-2 px-6 mt-4 rounded hover:bg-indigo-500 duration-300">
+        {/* <button className="bg-indigo-600 text-white py-2 px-6 mt-4 rounded hover:bg-indigo-500 duration-300">
           Get Started
+        </button> */}
+
+        <button
+          className="flex select-none bg-indigo-600 items-center gap-2 rounded-lg py-3 px-6 mt-4 text-center align-middle
+           font-sans text-md font-bold uppercase text-white transition-all shadow:md hover:shadow-lg
+         hover:bg-indigo-500/100 active:bg-indigo-700 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none duration-500"
+          type="button"
+          data-ripple-dark="true"
+        >
+          Get Started
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            aria-hidden="true"
+            className="h-5 w-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+            ></path>
+          </svg>
         </button>
       </div>
     </main>
