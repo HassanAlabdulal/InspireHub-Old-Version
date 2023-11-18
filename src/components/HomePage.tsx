e06a632a69dee535b265f213de3c1e16928cba5c
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-19 02:06:53 +0300

    Adjust margins

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 export default function HomePage() {
   return (
-    <main className="flex items-center justify-center w-full h-screen gap-3 mt-10 max-md:mt-30">
+    <main className="flex items-center justify-center w-full h-screen gap-3 max-md:mt-30">
       <div className="flex flex-col items-center justify-center">
         <div className=" w-96 h-96 max-xl:w-80 max-xl:h-80 max-sm:mt-16 max-lg:mt-24">
           <Lottie animationData={animationData} />
