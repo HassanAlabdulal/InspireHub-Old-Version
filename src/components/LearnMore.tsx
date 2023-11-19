2345fb658fe0d1ab397e10b305f45ce418aa6015
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-19 18:46:17 +0300

    Change background color

--- src/components/LearnMore.tsx
+++ src/components/LearnMore.tsx
@@ -1,6 +1,6 @@
 export default function LearnMoreComponent() {
   return (
-    <article id="learn-more" className="h-screen c">
+    <article id="learn-more" className="h-screen bg-[#f7f7f7]">
       <h1>hello</h1>
     </article>
   );
