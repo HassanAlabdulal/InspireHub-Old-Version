23014208d3e2f74ef9b1a2916ce8258d8bbb016d
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-19 18:45:43 +0300

    Chnage background color

--- src/components/LearnMore.tsx
+++ src/components/LearnMore.tsx
@@ -1,6 +1,6 @@
 export default function LearnMoreComponent() {
   return (
-    <article id="learn-more" className="h-screen bg-green-400">
+    <article id="learn-more" className="h-screen c">
       <h1>hello</h1>
     </article>
   );
