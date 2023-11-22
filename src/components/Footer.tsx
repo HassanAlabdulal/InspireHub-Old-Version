719f3a51ccb0e7d783497edee8cbe5dd22b636eb
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-22 21:29:46 +0300

    Remove flowbite link from href

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="w-full max-w-screen-xl p-4 mx-auto md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
-            href="https://flowbite.com/"
+            href="/"
             className="flex items-center mb-4 space-x-3 sm:mb-0 rtl:space-x-reverse"
           >
             <img
@@ -37,7 +37,7 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center ">
           © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="/" className="hover:underline">
             InspireHub™
           </a>
           . All Rights Reserved.
