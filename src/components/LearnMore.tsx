f682c3441d07ece8b1855abbd53742eca46a9a02
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-26 09:13:42 +0300

    Link a button in learn more page to sign up page

--- src/components/LearnMore.tsx
+++ src/components/LearnMore.tsx
@@ -10,13 +10,13 @@ export default function LearnMore() {
         id="learn-more"
         className="py-8 text-[#bfa260] bg-[#f7f7f7] font-nunito"
       >
-        <h1 className="mt-16 mx-6 text-2xl md:text-3xl xl:text-4xl font-extrabold text-center uppercase tracking-loose">
+        <h1 className="mx-6 mt-16 text-2xl font-extrabold text-center uppercase md:text-3xl xl:text-4xl tracking-loose">
           Discover, Collaborate, & Innovate
         </h1>
-        <div className="px-4 py-0 flex flex-col-reverse items-center justify-center md:flex-row md:px-6 md:py-12 lg:px-8">
+        <div className="flex flex-col-reverse items-center justify-center px-4 py-0 md:flex-row md:px-6 md:py-12 lg:px-8">
           {/* Left side content */}
           <div className="flex flex-col w-full px-4 md:w-6/12 lg:w-5/12 xl:w-4/12">
-            <p className="mb-1 text-2xl leading-normal md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2 font-semibold">
+            <p className="mb-1 text-2xl font-semibold leading-normal md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
               What is InspireHub?
             </p>
             <p className="mb-6 font-roboto text-base text-[#121212] md:text-lg lg:text-xl">
@@ -26,7 +26,7 @@ export default function LearnMore() {
               screen and into the world, where feedback becomes the forge for
               excellence, and where AI meets human ingenuity to spark new ideas.
             </p>
-            <p className="mb-1 text-2xl leading-normal md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2 font-semibold">
+            <p className="mb-1 text-2xl font-semibold leading-normal md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
               Our Mission
             </p>
             <p className="mb-6 font-roboto text-base text-[#121212] md:text-lg lg:text-xl">
@@ -34,7 +34,7 @@ export default function LearnMore() {
               reality, foster an ecosystem of feedback that nurtures growth, and
               unlock opportunities that connect talent with the market.
             </p>
-            <p className="mb-1 text-2xl leading-normal md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2 font-semibold">
+            <p className="mb-1 text-2xl font-semibold leading-normal md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
               Join Us
             </p>
             <p className="mb-6 font-roboto text-base text-[#121212] md:text-lg lg:text-xl">
@@ -43,6 +43,7 @@ export default function LearnMore() {
             </p>
             <div className="flex items-center gap-3">
               <a
+                href="SignUpPage"
                 className="flex select-none items-center justify-center cursor-pointer
                  rounded-lg bg-[#5f7fbf] border-2 border-[#5f7fbf] px-[1.693rem]
                    py-2 text-base font-bold text-white align-middle
@@ -77,7 +78,7 @@ export default function LearnMore() {
           id="features"
           className="flex flex-col items-center justify-center mt-32 mb-8"
         >
-          <h1 className="mt-24 mb-16 mx-6 text-2xl md:text-3xl xl:text-4xl font-extrabold text-center uppercase tracking-loose">
+          <h1 className="mx-6 mt-24 mb-16 text-2xl font-extrabold text-center uppercase md:text-3xl xl:text-4xl tracking-loose">
             Our Features
           </h1>
           <div className="relative ml-0 md:my-8 lg:w-10/12">
@@ -93,10 +94,10 @@ export default function LearnMore() {
                 <div className="flex flex-row-reverse items-center justify-between w-full mb-8 left-timeline">
                   <div className="order-1 w-5/12"></div>
                   <div className="order-1 w-5/12 px-1 py-4 text-left">
-                    <h4 className="mb-1 text-2xl leading-normal font-semibold text-left md:text-center md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
+                    <h4 className="mb-1 text-2xl font-semibold leading-normal text-left md:text-center md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
                       Showcase Your Genius
                     </h4>
-                    <p className="text-base leading-snug font-roboto text-left md:text-center text-black text-opacity-100 md:text-lg lg:text-xl">
+                    <p className="text-base leading-snug text-left text-black text-opacity-100 font-roboto md:text-center md:text-lg lg:text-xl">
                       Unveil your creative prowess on InspireHub, where each
                       project is a beacon to industry pioneers and visionaries.
                       Share your work, captivate an audience of leaders, and let
@@ -110,10 +111,10 @@ export default function LearnMore() {
                   <div></div>
                   <div className="order-1 w-5/12"></div>
                   <div className="order-1 w-5/12 px-1 py-4 text-left">
-                    <h4 className="mb-1 text-2xl leading-normal font-semibold text-left md:text-center md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
+                    <h4 className="mb-1 text-2xl font-semibold leading-normal text-left md:text-center md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
                       Thrive on Collaboration
                     </h4>
-                    <p className="text-base leading-snug font-roboto text-left md:text-center text-black text-opacity-100 md:text-lg lg:text-xl">
+                    <p className="text-base leading-snug text-left text-black text-opacity-100 font-roboto md:text-center md:text-lg lg:text-xl">
                       Thrive on Collaboration Dive into a collaborative oasis
                       where feedback refines and perfects. Engage with a
                       community where each critique enriches your work, ensuring
@@ -126,10 +127,10 @@ export default function LearnMore() {
                 <div className="flex flex-row-reverse items-center justify-between w-full mb-8 left-timeline">
                   <div className="order-1 w-5/12"></div>
                   <div className="order-1 w-5/12 px-1 py-4 text-left">
-                    <h4 className="mb-1 text-2xl leading-normal font-semibold text-left md:text-center md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
+                    <h4 className="mb-1 text-2xl font-semibold leading-normal text-left md:text-center md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
                       Fuel Your Creativity
                     </h4>
-                    <p className="text-base leading-snug font-roboto text-left md:text-center text-black text-opacity-100 md:text-lg lg:text-xl">
+                    <p className="text-base leading-snug text-left text-black text-opacity-100 font-roboto md:text-center md:text-lg lg:text-xl">
                       Spark your imagination with our AI-driven muse, crafting
                       bespoke project suggestions that marry your interests with
                       the pulse of current trends. It's the creative catalyst
@@ -142,10 +143,10 @@ export default function LearnMore() {
                 <div className="flex items-center justify-between w-full mb-8 right-timeline">
                   <div className="order-1 w-5/12"></div>
                   <div className="order-1 w-5/12 px-1 py-4">
-                    <h4 className="mb-1 text-2xl leading-normal font-semibold text-left md:text-center md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
+                    <h4 className="mb-1 text-2xl font-semibold leading-normal text-left md:text-center md:text-3xl md:leading-relaxed lg:text-4xl lg:mb-2">
                       Rise to the Top
                     </h4>
-                    <p className="text-base leading-snug font-roboto text-left md:text-center text-black text-opacity-100 md:text-lg lg:text-xl">
+                    <p className="text-base leading-snug text-left text-black text-opacity-100 font-roboto md:text-center md:text-lg lg:text-xl">
                       Rise to the Top Ascend the creative ladder with our
                       intuitive rating system, designed to showcase your
                       project's impact and propel your work into the spotlight.
@@ -157,7 +158,7 @@ export default function LearnMore() {
               </div>
             </div>
             <img
-              className="mx-auto -mt-10 w-6/12"
+              className="w-6/12 mx-auto -mt-10"
               src="src/assets/undraw_launching_re_tomg.svg"
             />
           </div>
