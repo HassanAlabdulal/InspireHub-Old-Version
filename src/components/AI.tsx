6ca79c7d01a08b67a95ba53a7e5c0a227640a1be
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-26 09:47:36 +0300

    Add extra inputs and drobpdowns in AI page

--- src/components/AI.tsx
+++ src/components/AI.tsx
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 
 interface InputProps {
   label: string;
+  placeholder: string;
   onChange: (value: string) => void;
 }
 
-const Input: React.FC<InputProps> = ({ label, onChange }) => (
+const Input: React.FC<InputProps> = ({ label, placeholder, onChange }) => (
   <div className="mb-4">
     <label className="block mb-2 text-sm font-bold text-gray-700">
       {label}
     </label>
     <input
       type="text"
+      placeholder={placeholder}
       onChange={(e) => onChange(e.target.value)}
       className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
     />
@@ -46,10 +48,14 @@ const AI: React.FC = () => {
   const [interests, setInterests] = useState({
     areasOfInterest: "",
     skillsOrTechnologies: "",
+    IndustryRelevance: "",
+    projectType: "",
   });
   const [preferences, setPreferences] = useState({
-    projectType: "",
     timeCommitment: "",
+    PreferredTools: "",
+    CollaborationPreference: "",
+    DifficultyLevel: "",
   });
   const [generatedIdeas, setGeneratedIdeas] = useState<string>("");
 
@@ -59,21 +65,73 @@ const AI: React.FC = () => {
   };
 
   return (
-    <div className="flex items-start justify-around min-h-screen p-10 bg-[#f7f7f7]">
+    <div className="flex items-center justify-around min-h-screen p-10 bg-[#f7f7f7]">
       <div className="flex-1">
         <h2 className="mb-4 text-lg font-semibold text-center">
           User Interests
         </h2>
         <Input
           label="Areas of Interest"
+          placeholder="Specific domains you are interested in, technology, health, etc.."
           onChange={(value) =>
             setInterests({ ...interests, areasOfInterest: value })
           }
         />
         <Input
           label="Skills or Technologies"
+          placeholder="Skills or technologies you're proficient in or wish to explore"
           onChange={(value) =>
             setInterests({ ...interests, skillsOrTechnologies: value })
+          }
+        />
+
+        <Dropdown
+          label="Industry Relevance"
+          options={[
+            { value: "technology", label: "Technology" },
+            { value: "healthcare", label: "Healthcare" },
+            { value: "education", label: "Education" },
+            { value: "finance", label: "Finance" },
+            { value: "automotive", label: "Automotive" },
+            { value: "entertainment", label: "Entertainment" },
+            { value: "manufacturing", label: "Manufacturing" },
+            { value: "agriculture", label: "Agriculture" },
+            { value: "retail", label: "Retail" },
+            { value: "energy", label: "Energy" },
+            { value: "environment", label: "Environment" },
+            { value: "food_and_beverage", label: "Food & Beverage" },
+            {
+              value: "government_and_public_sector",
+              label: "Government & Public Sector",
+            },
+            { value: "real_estate", label: "Real Estate" },
+            {
+              value: "transportation_and_logistics",
+              label: "Transportation & Logistics",
+            },
+            { value: "telecommunications", label: "Telecommunications" },
+            { value: "pharmaceuticals", label: "Pharmaceuticals" },
+            { value: "space", label: "Space" },
+            { value: "cyber_security", label: "Cyber Security" },
+            { value: "art_and_design", label: "Art & Design" },
+
+            // More options...
+          ]}
+          onSelect={(value) =>
+            setInterests({ ...interests, IndustryRelevance: value })
+          }
+        />
+
+        <Dropdown
+          label="Project Type"
+          options={[
+            { value: "development", label: "Development" },
+            { value: "research", label: "Research" },
+            { value: "entrepreneurship", label: "Entrepreneurship" },
+            // More options...
+          ]}
+          onSelect={(value) =>
+            setInterests({ ...interests, projectType: value })
           }
         />
       </div>
@@ -102,18 +160,7 @@ const AI: React.FC = () => {
         <h2 className="mb-4 text-lg font-semibold text-center">
           User Preferences
         </h2>
-        <Dropdown
-          label="Project Type"
-          options={[
-            { value: "research", label: "Research" },
-            { value: "development", label: "Development" },
-            { value: "entrepreneurship", label: "Entrepreneurship" },
-            // More options...
-          ]}
-          onSelect={(value) =>
-            setPreferences({ ...preferences, projectType: value })
-          }
-        />
+
         <Dropdown
           label="Time Commitment"
           options={[
@@ -124,6 +171,38 @@ const AI: React.FC = () => {
           ]}
           onSelect={(value) =>
             setPreferences({ ...preferences, timeCommitment: value })
+          }
+        />
+
+        <Dropdown
+          label="Difficulty Level"
+          options={[
+            { value: "easy", label: "Easy" },
+            { value: "medium", label: "Medium" },
+            { value: "hard", label: "Hard" },
+            // More options...
+          ]}
+          onSelect={(value) =>
+            setPreferences({ ...preferences, DifficultyLevel: value })
+          }
+        />
+        <Dropdown
+          label="Collaboration Preference:"
+          options={[
+            { value: "solo", label: "Solo" },
+            { value: "team", label: "Team" },
+            // More options...
+          ]}
+          onSelect={(value) =>
+            setPreferences({ ...preferences, DifficultyLevel: value })
+          }
+        />
+
+        <Input
+          label="Preferred Tools"
+          placeholder=" Write down the tools or frameworks you prefer to use"
+          onChange={(value) =>
+            setPreferences({ ...preferences, PreferredTools: value })
           }
         />
       </div>
