ef1061299c69df7c0341f75d908d6bbf1eb1d86b
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-26 21:45:30 +0300

    Enhace the layout of AI page

--- src/components/AI.tsx
+++ src/components/AI.tsx
@@ -67,169 +67,174 @@ const AI: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto min-h-screen p-6 bg-[#f7f7f7]">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-        <div className="p-6 bg-white rounded-lg shadow-md">
-          <h2 className="mb-4 text-lg font-semibold text-center">
-            User Interests
-          </h2>
-          <Input
-            label="Areas of Interest"
-            placeholder="Specific domains you are interested in, technology, health, etc.."
-            onChange={(value) =>
-              setInterests({ ...interests, areasOfInterest: value })
-            }
-          />
-          <Input
-            label="Skills or Technologies"
-            placeholder="Skills or technologies you're proficient in or wish to explore"
-            onChange={(value) =>
-              setInterests({ ...interests, skillsOrTechnologies: value })
-            }
-          />
+    <div className="min-h-screen bg-[#f7f7f7] flex justify-center items-center">
+      <div className="container p-8 mx-auto bg-white rounded-lg ">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+          <div className="p-6 bg-[#f7f7f7] rounded-lg shadow-md">
+            <h2 className="mb-4 text-lg font-semibold text-center">
+              User Interests
+            </h2>
+            <Input
+              label="Areas of Interest"
+              placeholder="Specific domains you are interested in, technology, health, etc.."
+              onChange={(value) =>
+                setInterests({ ...interests, areasOfInterest: value })
+              }
+            />
+            <Input
+              label="Skills or Technologies"
+              placeholder="Skills or technologies you're proficient in or wish to explore"
+              onChange={(value) =>
+                setInterests({ ...interests, skillsOrTechnologies: value })
+              }
+            />
 
-          <Dropdown
-            label="Industry Relevance"
-            options={[
-              { value: "technology", label: "Technology" },
-              { value: "healthcare", label: "Healthcare" },
-              { value: "education", label: "Education" },
-              { value: "finance", label: "Finance" },
-              { value: "automotive", label: "Automotive" },
-              { value: "entertainment", label: "Entertainment" },
-              { value: "manufacturing", label: "Manufacturing" },
-              { value: "agriculture", label: "Agriculture" },
-              { value: "retail", label: "Retail" },
-              { value: "energy", label: "Energy" },
-              { value: "environment", label: "Environment" },
-              { value: "food_and_beverage", label: "Food & Beverage" },
-              {
-                value: "government_and_public_sector",
-                label: "Government & Public Sector",
-              },
-              { value: "real_estate", label: "Real Estate" },
-              {
-                value: "transportation_and_logistics",
-                label: "Transportation & Logistics",
-              },
-              { value: "telecommunications", label: "Telecommunications" },
-              { value: "pharmaceuticals", label: "Pharmaceuticals" },
-              { value: "space", label: "Space" },
-              { value: "cyber_security", label: "Cyber Security" },
-              { value: "art_and_design", label: "Art & Design" },
+            <Dropdown
+              label="Industry Relevance"
+              options={[
+                { value: "technology", label: "Technology" },
+                { value: "healthcare", label: "Healthcare" },
+                { value: "education", label: "Education" },
+                { value: "finance", label: "Finance" },
+                { value: "automotive", label: "Automotive" },
+                { value: "entertainment", label: "Entertainment" },
+                { value: "manufacturing", label: "Manufacturing" },
+                { value: "agriculture", label: "Agriculture" },
+                { value: "retail", label: "Retail" },
+                { value: "energy", label: "Energy" },
+                { value: "environment", label: "Environment" },
+                { value: "food_and_beverage", label: "Food & Beverage" },
+                {
+                  value: "government_and_public_sector",
+                  label: "Government & Public Sector",
+                },
+                { value: "real_estate", label: "Real Estate" },
+                {
+                  value: "transportation_and_logistics",
+                  label: "Transportation & Logistics",
+                },
+                { value: "telecommunications", label: "Telecommunications" },
+                { value: "pharmaceuticals", label: "Pharmaceuticals" },
+                { value: "space", label: "Space" },
+                { value: "cyber_security", label: "Cyber Security" },
+                { value: "art_and_design", label: "Art & Design" },
 
-              // More options...
-            ]}
-            onSelect={(value) =>
-              setInterests({ ...interests, IndustryRelevance: value })
-            }
-          />
+                // More options...
+              ]}
+              onSelect={(value) =>
+                setInterests({ ...interests, IndustryRelevance: value })
+              }
+            />
 
-          <Dropdown
-            label="Project Type"
-            options={[
-              { value: "development", label: "Development" },
-              { value: "research", label: "Research" },
-              { value: "entrepreneurship", label: "Entrepreneurship" },
-              { value: "social_impact", label: "Social Impact" },
-              { value: "educational", label: "Educational" },
-              { value: "creative_arts", label: "Creative Arts" },
-              {
-                value: "technology_innovation",
-                label: "Technology Innovation",
-              },
-              { value: "scientific_inquiry", label: "Scientific Inquiry" },
-              { value: "community_service", label: "Community Service" },
-              {
-                value: "environmental_conservation",
-                label: "Environmental Conservation",
-              },
-              { value: "health_and_wellness", label: "Health and Wellness" },
-              { value: "data_analysis", label: "Data Analysis" },
-              { value: "software_development", label: "Software Development" },
-              { value: "product_design", label: "Product Design" },
-              { value: "market_research", label: "Market Research" },
-              { value: "business_strategy", label: "Business Strategy" },
-              { value: "engineering", label: "Engineering" },
-              {
-                value: "multimedia_production",
-                label: "Multimedia Production",
-              },
-              { value: "event_planning", label: "Event Planning" },
-              { value: "policy_development", label: "Policy Development" },
-            ]}
-            onSelect={(value) =>
-              setInterests({ ...interests, projectType: value })
-            }
-          />
-        </div>
-
-        <div className="flex flex-col items-center justify-center p-6 bg-white rounded-lg shadow-md md:col-span-1">
-          <div className="w-full h-full max-xl:w-80 max-xl:h-80 max-lg:mt-24">
-            <Lottie animationData={animationData} />
+            <Dropdown
+              label="Project Type"
+              options={[
+                { value: "development", label: "Development" },
+                { value: "research", label: "Research" },
+                { value: "entrepreneurship", label: "Entrepreneurship" },
+                { value: "social_impact", label: "Social Impact" },
+                { value: "educational", label: "Educational" },
+                { value: "creative_arts", label: "Creative Arts" },
+                {
+                  value: "technology_innovation",
+                  label: "Technology Innovation",
+                },
+                { value: "scientific_inquiry", label: "Scientific Inquiry" },
+                { value: "community_service", label: "Community Service" },
+                {
+                  value: "environmental_conservation",
+                  label: "Environmental Conservation",
+                },
+                { value: "health_and_wellness", label: "Health and Wellness" },
+                { value: "data_analysis", label: "Data Analysis" },
+                {
+                  value: "software_development",
+                  label: "Software Development",
+                },
+                { value: "product_design", label: "Product Design" },
+                { value: "market_research", label: "Market Research" },
+                { value: "business_strategy", label: "Business Strategy" },
+                { value: "engineering", label: "Engineering" },
+                {
+                  value: "multimedia_production",
+                  label: "Multimedia Production",
+                },
+                { value: "event_planning", label: "Event Planning" },
+                { value: "policy_development", label: "Policy Development" },
+              ]}
+              onSelect={(value) =>
+                setInterests({ ...interests, projectType: value })
+              }
+            />
           </div>
-          <textarea
-            value={generatedIdeas}
-            onChange={(e) => setGeneratedIdeas(e.target.value)}
-            className="w-full h-32 p-4 border rounded shadow resize-none"
-            placeholder="Generated project ideas will appear here..."
-          />
-          <button
-            onClick={getProjectIdeas}
-            className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-          >
-            Get Project Ideas
-          </button>
-        </div>
 
-        <div className="p-6 bg-white rounded-lg shadow-md">
-          <h2 className="mb-4 text-lg font-semibold text-center">
-            User Preferences
-          </h2>
+          <div className="flex flex-col items-center justify-center p-6 bg-[#f7f7f7] rounded-lg shadow-md md:col-span-1">
+            <div className="w-full h-full max-xl:w-80 max-xl:h-80 max-lg:mt-24">
+              <Lottie animationData={animationData} />
+            </div>
+            <textarea
+              value={generatedIdeas}
+              onChange={(e) => setGeneratedIdeas(e.target.value)}
+              className="w-full h-32 p-4 border rounded shadow resize-none"
+              placeholder="Generated project ideas will appear here..."
+            />
+            <button
+              onClick={getProjectIdeas}
+              className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+            >
+              Get Project Ideas
+            </button>
+          </div>
 
-          <Input
-            label="Preferred Tools"
-            placeholder=" Write down the tools or frameworks you prefer to use"
-            onChange={(value) =>
-              setPreferences({ ...preferences, PreferredTools: value })
-            }
-          />
-          <Dropdown
-            label="Time Commitment"
-            options={[
-              { value: "weeks", label: "Weeks" },
-              { value: "months", label: "Months" },
-              { value: "ongoing", label: "Ongoing" },
-              // More options...
-            ]}
-            onSelect={(value) =>
-              setPreferences({ ...preferences, timeCommitment: value })
-            }
-          />
+          <div className="p-6 bg-[#f7f7f7] rounded-lg shadow-md">
+            <h2 className="mb-4 text-lg font-semibold text-center">
+              User Preferences
+            </h2>
 
-          <Dropdown
-            label="Difficulty Level"
-            options={[
-              { value: "easy", label: "Easy" },
-              { value: "medium", label: "Medium" },
-              { value: "hard", label: "Hard" },
-              // More options...
-            ]}
-            onSelect={(value) =>
-              setPreferences({ ...preferences, DifficultyLevel: value })
-            }
-          />
-          <Dropdown
-            label="Collaboration Preference:"
-            options={[
-              { value: "solo", label: "Solo" },
-              { value: "team", label: "Team" },
-              // More options...
-            ]}
-            onSelect={(value) =>
-              setPreferences({ ...preferences, DifficultyLevel: value })
-            }
-          />
+            <Input
+              label="Preferred Tools"
+              placeholder=" Write down the tools or frameworks you prefer to use"
+              onChange={(value) =>
+                setPreferences({ ...preferences, PreferredTools: value })
+              }
+            />
+            <Dropdown
+              label="Time Commitment"
+              options={[
+                { value: "weeks", label: "Weeks" },
+                { value: "months", label: "Months" },
+                { value: "ongoing", label: "Ongoing" },
+                // More options...
+              ]}
+              onSelect={(value) =>
+                setPreferences({ ...preferences, timeCommitment: value })
+              }
+            />
+
+            <Dropdown
+              label="Difficulty Level"
+              options={[
+                { value: "easy", label: "Easy" },
+                { value: "medium", label: "Medium" },
+                { value: "hard", label: "Hard" },
+                // More options...
+              ]}
+              onSelect={(value) =>
+                setPreferences({ ...preferences, DifficultyLevel: value })
+              }
+            />
+            <Dropdown
+              label="Collaboration Preference:"
+              options={[
+                { value: "solo", label: "Solo" },
+                { value: "team", label: "Team" },
+                // More options...
+              ]}
+              onSelect={(value) =>
+                setPreferences({ ...preferences, DifficultyLevel: value })
+              }
+            />
+          </div>
         </div>
       </div>
     </div>
