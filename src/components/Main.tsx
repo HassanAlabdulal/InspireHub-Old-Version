7e0f61a32fcd6ba8f04f5c8fea39b84b35c0d424
l3b3al <alabdulaal.alii@gmail.com>
2023-11-28 08:13:52 +0300

    Redesigning main page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -75,10 +75,26 @@ export default function Main() {
     console.log(`Navigate to details of project with id: ${projectId}`);
   };
   return (
-    <div className="bg-[#f7f7f7] pt-[65px] flex flex-col items-center min-h-screen font-roboto">
-      <h1 className="text-[#bfa260] font-roboto font-bold text-3xl mb-6">
-        Main Page
-      </h1>
+    <div className="bg-[#f7f7f7] pt-20 flex flex-col items-center min-h-screen font-roboto">
+      <div className="flex flex-col items-center">
+        <h1 className="mx-6 text-2xl font-bold font-nunito text-center md:text-3xl xl:text-4xl tracking-loose">
+          Search Projects
+        </h1>
+
+        <form>
+          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+              </svg>
+            </div>
+            <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+            <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          </div>
+        </form>
+
+      </div>
       <div className="flex items-center justify-end w-full px-6">
         <a
           href="UploadProjectPage"
