e425d2dafe25d0eb7f6d2a76d160ec52b37836b5
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-29 12:27:10 +0300

    Build the structure of show project page

--- src/components/ShowProject.tsx
+++ src/components/ShowProject.tsx
@@ -1,77 +1,133 @@
-import React from 'react';
+import React from "react";
 
-const projectData = {
+interface TeamMember {
+  name: string;
+  linkedIn: string;
+  twitter: string;
+}
+
+interface ProjectData {
+  title: string;
+  images: string[];
+  date: string;
+  category: string;
+  budget: string;
+  currency: string;
+  description: string;
+  motivation: string;
+  teamMembers: TeamMember[];
+  features: string;
+  resources: string;
+  tools: string;
+  others: string;
+}
+
+const projectData: ProjectData = {
   title: "InspireHub",
-  images: [
-    "src/assets/ContactUs.png", 
-  ],
+  images: ["src/assets/ContactUs.png"],
   date: "2023-01-01",
   category: "Technology",
   budget: "5000",
   currency: "SAR",
-  description: "It is a website that is a hub for inspiring and innovative project ideas...",
-  motivation: "The primary motivation behind InspireHub is to address a significant challenge...",
+  description:
+    "It is a website that is a hub for inspiring and innovative project ideas...",
+  motivation:
+    "The primary motivation behind InspireHub is to address a significant challenge...",
   teamMembers: [
     { name: "Abdullah", linkedIn: "aamhaamm", twitter: "aamhaamm" },
   ],
   features: "Easy to use, Suitable for everyone ...",
   resources: "https://github.com/InspireHub",
   tools: "React, Astro ...",
-  others: "This is the first version, we are working to update it as soon as depending on your suggestions...",
+  others:
+    "This is the first version, we are working to update it as soon as depending on your suggestions...",
 };
 
+const ShowProject: React.FC = () => {
+  return (
+    <div className="container p-6 mx-auto">
+      <header className="mb-6 text-center">
+        <h1 className="text-4xl font-bold">{projectData.title}</h1>
+      </header>
 
-export default function ShowProject() {
-    return (
-      <div className="bg-[#f7f7f7] pt-[65px] flex flex-col items-center min-h-screen font-roboto">
-        <div className="bg-[#f7f7f7] p-10 shadow-2xl rounded max-w-4xl w-full">
-          {/* Title Section */}
-          <h1 className="text-[#bfa260] font-bold text-3xl mb-6 text-center">
-            {projectData.title}
-          </h1>
-  
-          {/* Image Section */}
-          {projectData.images.length > 0 && (
-            <div className="mb-6">
-              <img
-                src={projectData.images[0]}
-                alt="Project"
-                className="w-full object-cover"
-                style={{ maxHeight: '500px' }}
-              />
-            </div>
-          )}
-  
-          {/* Project Details Section */}
-          <h2 className="text-[#bfa260] font-bold text-2xl mb-4">
-            Project Details
-          </h2>
-          <div className="text-black">
-            <p className="mb-2"><strong className="text-[#bfa260]" >Date of project:</strong> <br /> {projectData.date}</p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Category:</strong> <br /> {projectData.category}</p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Budget:</strong> <br /> {`${projectData.budget} ${projectData.currency}`}</p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Description:</strong> <br /> {projectData.description}</p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Motivation:</strong> <br /> {projectData.motivation}</p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Team member:</strong> <br /> 
-            {projectData.teamMembers.map((member, index) => (
-              <div key={index} className="mb-2">
-                <p><strong className="text-[#bfa260]">Name:</strong> {member.name}</p>
-                <p>
-                  <strong className="text-[#bfa260]">LinkedIn:</strong> <a href={`https://${member.linkedIn}`} className="text-[#5f7fbf] font-bold hover:underline" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a>
-                </p>
-                <p>
-                  <strong className="text-[#bfa260]">Twitter:</strong> <a href={`https://${member.twitter}`} className="text-[#5f7fbf] font-bold hover:underline" target="_blank" rel="noopener noreferrer">Twitter Profile</a>
-                </p>
-              </div>
+      <section className="mb-6">
+        <div className="flex flex-wrap justify-center gap-4">
+          {projectData.images.map((imageSrc, index) => (
+            <img
+              key={index}
+              className="rounded-lg shadow-lg h-96 w-96"
+              src={imageSrc}
+              alt={`Project ${projectData.title}`}
+            />
           ))}
-          </p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Features:</strong> <br /> {projectData.features}</p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Resources:</strong> <br /> <a href={projectData.resources} target="_blank" rel="noopener noreferrer">{projectData.resources}</a></p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Tools:</strong> <br /> {projectData.tools}</p>
-            <p className="mb-2"><strong className="text-[#bfa260]">Others:</strong> <br /> {projectData.others}</p>
-          </div>
-  
         </div>
-      </div>
-    );
-  }
+      </section>
+
+      <section className="mb-6">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+          <div>
+            <p className="font-semibold">
+              Date: <span className="font-normal">{projectData.date}</span>
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold">
+              Category:{" "}
+              <span className="font-normal">{projectData.category}</span>
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold">
+              Budget:{" "}
+              <span className="font-normal">{`${projectData.budget} ${projectData.currency}`}</span>
+            </p>
+          </div>
+        </div>
+        <p className="mt-4">{projectData.description}</p>
+        <p className="mt-4">
+          <strong>Motivation:</strong> {projectData.motivation}
+        </p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="mb-4 text-2xl font-semibold">Team Members</h2>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          {projectData.teamMembers.map((member, index) => (
+            <div key={index} className="p-4 bg-white rounded-lg shadow-lg">
+              <p className="font-semibold">{member.name}</p>
+              <p>LinkedIn: {member.linkedIn}</p>
+              <p>Twitter: {member.twitter}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="mb-4 text-2xl font-semibold">Features</h2>
+        <p>{projectData.features}</p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="mb-4 text-2xl font-semibold">Resources</h2>
+        <a
+          href={projectData.resources}
+          className="text-blue-600 hover:text-blue-800"
+        >
+          {projectData.resources}
+        </a>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="mb-4 text-2xl font-semibold">Tools Used</h2>
+        <p>{projectData.tools}</p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="mb-4 text-2xl font-semibold">Other Information</h2>
+        <p>{projectData.others}</p>
+      </section>
+    </div>
+  );
+};
+
+export default ShowProject;
