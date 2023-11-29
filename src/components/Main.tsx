d22e0b4d62bd043ae83abd33214ab43a272c7294
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-11-29 21:05:55 +0300

    Link renderStars component to main page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,6 @@
+import { renderStars } from "../utils/renderStars";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faStar as solidStar,
-  faStarHalfAlt,
-  faArrowRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
-import React from "react";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 type Project = {
   id: number;
@@ -39,37 +34,37 @@ export default function Main() {
     },
   ];
 
-  const renderStars = (rate: number) => {
-    let stars: JSX.Element[] = [];
-    for (let i = 1; i <= 5; i++) {
-      if (i <= rate) {
-        stars.push(
-          <FontAwesomeIcon
-            key={`full_${i}`}
-            icon={solidStar}
-            className="text-yellow-500"
-          />
-        );
-      } else if (i === Math.ceil(rate) && rate % 1 !== 0) {
-        stars.push(
-          <FontAwesomeIcon
-            key={`half_${i}`}
-            icon={faStarHalfAlt}
-            className="text-yellow-500"
-          />
-        );
-      } else {
-        stars.push(
-          <FontAwesomeIcon
-            key={`empty_${i}`}
-            icon={regularStar}
-            className="text-yellow-500"
-          />
-        );
-      }
-    }
-    return stars;
-  };
+  // const renderStars = (rate: number) => {
+  //   let stars: JSX.Element[] = [];
+  //   for (let i = 1; i <= 5; i++) {
+  //     if (i <= rate) {
+  //       stars.push(
+  //         <FontAwesomeIcon
+  //           key={`full_${i}`}
+  //           icon={solidStar}
+  //           className="text-yellow-500"
+  //         />
+  //       );
+  //     } else if (i === Math.ceil(rate) && rate % 1 !== 0) {
+  //       stars.push(
+  //         <FontAwesomeIcon
+  //           key={`half_${i}`}
+  //           icon={faStarHalfAlt}
+  //           className="text-yellow-500"
+  //         />
+  //       );
+  //     } else {
+  //       stars.push(
+  //         <FontAwesomeIcon
+  //           key={`empty_${i}`}
+  //           icon={regularStar}
+  //           className="text-yellow-500"
+  //         />
+  //       );
+  //     }
+  //   }
+  //   return stars;
+  // };
 
   const navigateToProjectDetails = (projectId: number) => {
     console.log(`Navigate to details of project with id: ${projectId}`);
@@ -77,23 +72,50 @@ export default function Main() {
   return (
     <div className="bg-[#f7f7f7] pt-20 flex flex-col items-center min-h-screen font-roboto">
       <div className="flex flex-col items-center">
-        <h1 className="mx-6 text-2xl font-bold font-nunito text-center md:text-3xl xl:text-4xl tracking-loose">
+        <h1 className="mx-6 text-2xl font-bold text-center font-nunito md:text-3xl xl:text-4xl tracking-loose">
           Search Projects
         </h1>
 
         <form>
-          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
           <div className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            <div className="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
               </svg>
             </div>
-            <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-            <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search Mockups, Logos..."
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
           </div>
         </form>
-
       </div>
       <div className="flex items-center justify-end w-full px-6">
         <a
--- src/utils/renderStars.tsx
+++ src/utils/renderStars.tsx
@@ -2,13 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faStar as solidStar,
   faStarHalfAlt,
-  faStar as regularStar,
 } from "@fortawesome/free-solid-svg-icons";
+import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
 
-export const renderStars = (rate: number): JSX.Element[] => {
+export const renderStars = (rate: number) => {
   let stars: JSX.Element[] = [];
   for (let i = 1; i <= 5; i++) {
     if (i <= rate) {
+      // This will add a full star for each integer less than or equal to the rate
       stars.push(
         <FontAwesomeIcon
           key={`full_${i}`}
@@ -17,6 +18,8 @@ export const renderStars = (rate: number): JSX.Element[] => {
         />
       );
     } else if (i === Math.ceil(rate) && rate % 1 !== 0) {
+      // This will add a half star only if the rate has a fractional part
+      // and we're at the next integer just above the rate
       stars.push(
         <FontAwesomeIcon
           key={`half_${i}`}
@@ -25,6 +28,7 @@ export const renderStars = (rate: number): JSX.Element[] => {
         />
       );
     } else {
+      // This adds an empty star for all other cases
       stars.push(
         <FontAwesomeIcon
           key={`empty_${i}`}
