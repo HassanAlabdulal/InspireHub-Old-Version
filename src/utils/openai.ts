a61ab9d6061786d9f624532eb3cfc63febd3e2ce
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-01 20:04:00 +0300

    test

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -4,5 +4,5 @@ const OPENAI_API_KEY = import.meta.env.PUBLIC_OPENAI_KEY;
 
 export const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true
+  dangerouslyAllowBrowser: true,
 });
