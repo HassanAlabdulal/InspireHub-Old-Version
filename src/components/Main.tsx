6878ece5d4fffd411ade170692633d41f5aed43b
l3b3al <alabdulaal.alii@gmail.com>
2023-12-01 23:33:17 +0300

    Added combobox for sirt by

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,6 +8,9 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
 import React from "react";
+import { useState } from 'react';
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/24/solid';
+import { Combobox } from '@headlessui/react';
 
 type Project = {
   id: number;
@@ -18,6 +21,15 @@ type Project = {
   description: string;
   imageUrl: string;
 };
+
+const people = [
+  { id: 1, name: 'Leslie Alexander' },
+  // More users...
+]
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function Main() {
   const projects: Project[] = [
@@ -44,6 +56,17 @@ export default function Main() {
   const navigateToProjectDetails = (projectId: number) => {
     console.log(`Navigate to details of project with id: ${projectId}`);
   };
+
+  const [query, setQuery] = useState('')
+  const [selectedPerson, setSelectedPerson] = useState()
+
+  const filteredPeople =
+    query === ''
+      ? people
+      : people.filter((person) => {
+        return person.name.toLowerCase().includes(query.toLowerCase())
+      })
+
   return (
     <div className="bg-[#f7f7f7] pt-20 flex flex-col items-center min-h-screen font-roboto">
       <div className="flex flex-col items-center w-full">
@@ -76,6 +99,54 @@ export default function Main() {
           </div>
         </div>
       </div>
+
+      <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson}>
+        <Combobox.Label className="block text-sm font-medium text-gray-700">Assigned to</Combobox.Label>
+        <div className="relative mt-1">
+          <Combobox.Input
+            className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+            onChange={(event) => setQuery(event.target.value)}
+            displayValue={(person) => person.name}
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+            <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          </Combobox.Button>
+
+          {filteredPeople.length > 0 && (
+            <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredPeople.map((person) => (
+                <Combobox.Option
+                  key={person.id}
+                  value={person}
+                  className={({ active }) =>
+                    classNames(
+                      'relative cursor-default select-none py-2 pl-8 pr-4',
+                      active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                    )
+                  }
+                >
+                  {({ active, selected }) => (
+                    <>
+                      <span className={classNames('block truncate', selected && 'font-semibold')}>{person.name}</span>
+
+                      {selected && (
+                        <span
+                          className={classNames(
+                            'absolute inset-y-0 left-0 flex items-center pl-1.5',
+                            active ? 'text-white' : 'text-indigo-600'
+                          )}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      )}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          )}
+        </div>
+      </Combobox>
 
       {/* Filters Section */}
       <div className="flex items-center justify-center my-6">
