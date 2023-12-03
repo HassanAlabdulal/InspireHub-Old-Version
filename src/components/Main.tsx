c9875a3f0ff377980e6aa135667c6f2f85f58d1c
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-03 09:52:04 +0300

    Navigate the user to the project page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,9 +8,9 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
 import React from "react";
-import { useState } from 'react';
-import { CheckIcon, ChevronDownIcon } from '@heroicons/react/24/solid';
-import { Combobox } from '@headlessui/react';
+import { useState } from "react";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+import { Combobox } from "@headlessui/react";
 
 type Project = {
   id: number;
@@ -23,13 +23,13 @@ type Project = {
 };
 
 const Sorts = [
-  { id: 1, sortBy: 'Highest Rating' },
-  { id: 2, sortBy: 'Highest Budget' },
-  { id: 3, sortBy: 'Lowest Budget' },
-]
+  { id: 1, sortBy: "Highest Rating" },
+  { id: 2, sortBy: "Highest Budget" },
+  { id: 3, sortBy: "Lowest Budget" },
+];
 
 function classNames(...classes: any[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Main() {
@@ -40,7 +40,8 @@ export default function Main() {
       date: "2023-05-15",
       rate: 4.5,
       creator: "John Doe",
-      description: "Redesign and modernize our e-commerce platform to improve user experience, increase conversion rates, and enhance security.",
+      description:
+        "Redesign and modernize our e-commerce platform to improve user experience, increase conversion rates, and enhance security.",
       imageUrl: "src/assets/ContactUs.png",
     },
     {
@@ -49,7 +50,8 @@ export default function Main() {
       date: "2023-08-20",
       rate: 4.2,
       creator: "Jane Smith",
-      description: "Develop an AI-powered chatbot to provide instant customer support, answer inquiries, and streamline communication.",
+      description:
+        "Develop an AI-powered chatbot to provide instant customer support, answer inquiries, and streamline communication.",
       imageUrl: "src/assets/ContactUs.png",
     },
     {
@@ -58,7 +60,8 @@ export default function Main() {
       date: "2023-04-10",
       rate: 4.8,
       creator: "Michael Johnson",
-      description: "Develop a comprehensive strategy to expand our market presence in emerging economies, focusing on growth and profitability.",
+      description:
+        "Develop a comprehensive strategy to expand our market presence in emerging economies, focusing on growth and profitability.",
       imageUrl: "src/assets/ContactUs.png",
     },
     {
@@ -67,7 +70,8 @@ export default function Main() {
       date: "2023-06-30",
       rate: 4.6,
       creator: "Emily Brown",
-      description: "Create financial models and forecasts to aid in decision-making, budgeting, and financial planning for the next fiscal year.",
+      description:
+        "Create financial models and forecasts to aid in decision-making, budgeting, and financial planning for the next fiscal year.",
       imageUrl: "src/assets/ContactUs.png",
     },
     {
@@ -76,7 +80,8 @@ export default function Main() {
       date: "2023-09-25",
       rate: 4.9,
       creator: "David Wilson",
-      description: "Rehabilitate an aging bridge to ensure structural integrity, safety, and longevity, incorporating modern engineering practices.",
+      description:
+        "Rehabilitate an aging bridge to ensure structural integrity, safety, and longevity, incorporating modern engineering practices.",
       imageUrl: "src/assets/ContactUs.png",
     },
     {
@@ -85,7 +90,8 @@ export default function Main() {
       date: "2023-07-12",
       rate: 4.7,
       creator: "Sophia Lee",
-      description: "Design and develop advanced robotics systems for industrial automation, improving efficiency and reducing production costs.",
+      description:
+        "Design and develop advanced robotics systems for industrial automation, improving efficiency and reducing production costs.",
       imageUrl: "src/assets/ContactUs.png",
     },
     {
@@ -94,7 +100,8 @@ export default function Main() {
       date: "2023-03-02",
       rate: 4.4,
       creator: "Daniel Clark",
-      description: "Redesign the brand identity, including logo, color scheme, and visual elements, to reflect our evolving brand image.",
+      description:
+        "Redesign the brand identity, including logo, color scheme, and visual elements, to reflect our evolving brand image.",
       imageUrl: "src/assets/ContactUs.png",
     },
     {
@@ -103,34 +110,39 @@ export default function Main() {
       date: "2023-10-15",
       rate: 4.3,
       creator: "Olivia Taylor",
-      description: "Revamp the user interface of our mobile app to enhance user-friendliness, aesthetics, and overall user satisfaction.",
+      description:
+        "Revamp the user interface of our mobile app to enhance user-friendliness, aesthetics, and overall user satisfaction.",
       imageUrl: "src/assets/ContactUs.png",
-    }
+    },
   ];
 
   const navigateToProjectDetails = (projectId: number) => {
-    console.log(`Navigate to details of project with id: ${projectId}`);
+    // console.log(`Navigate to details of project with id: ${projectId}`);
+    window.location.href = "/ShowProjectPage/";
   };
 
-  const [query, setQuery] = useState('')
-  const [selectedSort, setSelectedSort] = useState()
+  const [query, setQuery] = useState("");
+  const [selectedSort, setSelectedSort] = useState();
 
   const filteredSorts =
-    query === ''
+    query === ""
       ? Sorts
       : Sorts.filter((Sorts) => {
-        return Sorts.sortBy.toLowerCase().includes(query.toLowerCase())
-      })
+          return Sorts.sortBy.toLowerCase().includes(query.toLowerCase());
+        });
 
   return (
     <div className="bg-[#f7f7f7] pt-20 flex flex-col items-center min-h-screen font-roboto gap-12">
       <div className="flex flex-col items-center w-full gap-6">
         <div className="flex flex-col gap-3">
-          <label htmlFor="name" className="text-center block text-xl font-bold font-nunito text-[#bfa260]">
+          <label
+            htmlFor="name"
+            className="text-center block text-xl font-bold font-nunito text-[#bfa260]"
+          >
             Search Projects
           </label>
           <div className="flex flex-row items-center gap-2">
-            <div className="w-full relative">
+            <div className="relative w-full">
               <input
                 type="text"
                 name="name"
@@ -138,57 +150,81 @@ export default function Main() {
                 className="shadow-sm focus:ring-[#3e60a3] focus:border-[#3e60a3] block w-60 h-12 text-base border-gray-300 pl-4 pr-14 rounded-full"
                 placeholder="Project"
               />
-              <a className="bg-[#5f7fbf] pt-2 pl-[10px] w-10 h-10 rounded-full absolute right-0 mr-1 -mt-11
+              <a
+                className="bg-[#5f7fbf] pt-2 pl-[10px] w-10 h-10 rounded-full absolute right-0 mr-1 -mt-11
               transition-all duration-700 hover:bg-[#3e60a3] focus:outline-none shadow-md hover:shadow-xl"
               >
-                <FontAwesomeIcon icon={faArrowRight} size="lg" className="text-[#f7f7f7]" />
+                <FontAwesomeIcon
+                  icon={faArrowRight}
+                  size="lg"
+                  className="text-[#f7f7f7]"
+                />
               </a>
             </div>
             <div>
-              <a
-                className="w-10 h-10 px-2 py-2 rounded-full border-2 border-[#5f7fbf] transition-all duration-500 hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
-              >
-                <FontAwesomeIcon icon={faSliders} size="lg" className="text-[#5f7fbf]" />
+              <a className="w-10 h-10 px-2 py-2 rounded-full border-2 border-[#5f7fbf] transition-all duration-500 hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                <FontAwesomeIcon
+                  icon={faSliders}
+                  size="lg"
+                  className="text-[#5f7fbf]"
+                />
               </a>
             </div>
           </div>
         </div>
         <div>
           <Combobox as="div" value={selectedSort} onChange={setSelectedSort}>
-            <Combobox.Label className="block text-base font-normal text-[#bfa260]">Sort by:</Combobox.Label>
+            <Combobox.Label className="block text-base font-normal text-[#bfa260]">
+              Sort by:
+            </Combobox.Label>
             <div className="relative mt-1">
               <Combobox.Input
                 className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-[#3e60a3] focus:outline-none focus:ring-1 focus:ring-[#3e60a3] sm:text-sm"
                 onChange={(event) => setQuery(event.target.value)}
                 displayValue={(sort: { sortBy: string }) => sort.sortBy}
               />
-              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-                <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center px-2 rounded-r-md focus:outline-none">
+                <ChevronDownIcon
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
               </Combobox.Button>
               {filteredSorts.length > 0 && (
-                <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Combobox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                   {filteredSorts.map((sort) => (
                     <Combobox.Option
                       key={sort.id}
                       value={sort}
                       className={({ active }) =>
                         classNames(
-                          'relative cursor-default select-none py-2 pl-8 pr-4',
-                          active ? 'bg-[#3e60a3] text-[#f7f7f7]' : 'text-[#121212]'
+                          "relative cursor-default select-none py-2 pl-8 pr-4",
+                          active
+                            ? "bg-[#3e60a3] text-[#f7f7f7]"
+                            : "text-[#121212]"
                         )
                       }
                     >
                       {({ active, selected }) => (
                         <>
-                          <span className={classNames('block truncate', selected && 'font-semibold')}>{sort.sortBy}</span>
+                          <span
+                            className={classNames(
+                              "block truncate",
+                              selected && "font-semibold"
+                            )}
+                          >
+                            {sort.sortBy}
+                          </span>
                           {selected && (
                             <span
                               className={classNames(
-                                'absolute inset-y-0 left-0 flex items-center pl-1.5',
-                                active ? 'text-[#f7f7f7]' : 'text-[#3e60a3]'
+                                "absolute inset-y-0 left-0 flex items-center pl-1.5",
+                                active ? "text-[#f7f7f7]" : "text-[#3e60a3]"
                               )}
                             >
-                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              <CheckIcon
+                                className="w-5 h-5"
+                                aria-hidden="true"
+                              />
                             </span>
                           )}
                         </>
@@ -203,22 +239,32 @@ export default function Main() {
       </div>
 
       {/* Projects Section */}
-      <ul role="list" className="grid grid-cols-1 gap-8 mx-8 sm:gap-12 sm:mx-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-8 mx-8 sm:gap-12 sm:mx-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+      >
         {projects.map((project) => (
           <li
             key={project.id}
             onClick={() => navigateToProjectDetails(project.id)}
-            className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200 transform transition duration-500 
-            hover:scale-105 hover:shadow-lg cursor-pointer"
+            className="flex flex-col col-span-1 text-center transition duration-500 transform bg-white divide-y divide-gray-200 rounded-lg shadow cursor-pointer hover:scale-105 hover:shadow-lg"
           >
-            <div className="flex-1 flex flex-col p-8">
-              <img className="w-full h-auto mx-auto rounded-lg" src={project.imageUrl} alt={project.title} />
-              <h3 className="mt-6 text-[#bfa260] text-base font-nunito font-bold">{project.title}</h3>
-              <dl className="mt-1 flex-grow flex flex-col justify-between gap-3">
+            <div className="flex flex-col flex-1 p-8">
+              <img
+                className="w-full h-auto mx-auto rounded-lg"
+                src={project.imageUrl}
+                alt={project.title}
+              />
+              <h3 className="mt-6 text-[#bfa260] text-base font-nunito font-bold">
+                {project.title}
+              </h3>
+              <dl className="flex flex-col justify-between flex-grow gap-3 mt-1">
                 <dt className="sr-only">Creator</dt>
-                <dd className="text-gray-500 text-sm">by {project.creator}</dd>
+                <dd className="text-sm text-gray-500">by {project.creator}</dd>
                 <dt className="sr-only">Description</dt>
-                <dd className="text-[#121212] text-sm line-clamp-3">{project.description}</dd>
+                <dd className="text-[#121212] text-sm line-clamp-3">
+                  {project.description}
+                </dd>
                 <dt className="sr-only">Rating</dt>
                 <dd className="mt-1">
                   <span className="px-2 py-1 text-[#AA8A41] text-xs font-medium bg-amber-100 rounded-full">
