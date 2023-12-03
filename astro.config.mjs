fa48c19546e6766b9435789df76b7d34e24df6bc
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-03 23:36:27 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,13 @@
-// import { defineConfig } from "astro/config";
-// import tailwind from "@astrojs/tailwind";
-// import react from "@astrojs/react";
-// import node from "@astrojs/node";
-
-// import vercel from "@astrojs/vercel/serverless";
-
-// export default defineConfig({
-//   integrations: [tailwind(), react()],
-//   output: "server",
-//   adapter: vercel()
-// });
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel(),
 });
