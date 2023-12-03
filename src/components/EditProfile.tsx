53b49933b83969e7e648956a5ae57e92f2eedd1d
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-03 22:17:16 +0300

    Minor changes in edit profile page

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function EditProfile() {
-  const [imagePreviewUrl, setImagePreviewUrl] = useState<string>('');
+  const [imagePreviewUrl, setImagePreviewUrl] = useState<string>("");
   const [inputKey, setInputKey] = useState<number | string>(Date.now());
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,7 +11,7 @@ export default function EditProfile() {
 
     if (file) {
       reader.onloadend = () => {
-        if (typeof reader.result === 'string') {
+        if (typeof reader.result === "string") {
           setImagePreviewUrl(reader.result);
         }
       };
@@ -20,20 +20,19 @@ export default function EditProfile() {
   };
 
   const handleRemoveImage = () => {
-    setImagePreviewUrl('');
+    setImagePreviewUrl("");
     setInputKey(Date.now()); // Reset the key to a new value
   };
 
   return (
     <div className="bg-[#f7f7f7] pt-[65px] flex flex-col items-center min-h-screen font-roboto">
-      <div className="w-full max-w-4xl m-auto bg-white rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row">
-        
+      <div className="flex flex-col w-full max-w-4xl m-auto overflow-hidden bg-white rounded-lg shadow-lg md:flex-row">
         {/* Left Column for Profile Photo */}
         <div className="md:w-1/2 bg-[#f7f7f7] flex flex-col items-center p-8">
-          <div className="w-64 h-64 rounded-full mb-4 overflow-hidden relative">
+          <div className="relative w-64 h-64 mb-4 overflow-hidden rounded-full">
             <img
-              className="w-full h-full object-cover"
-              src={imagePreviewUrl || 'src/assets/photo.png'}
+              className="object-cover w-full h-full"
+              src={imagePreviewUrl || "src/assets/photo.png"}
               alt="Profile"
             />
           </div>
@@ -67,88 +66,103 @@ export default function EditProfile() {
           )}
         </div>
 
-          {/* Right Column for Form */}
-          <div className="md:w-2/3 p-8">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2" htmlFor="grid-first-name">
-                  First Name
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="First Name"
-                />
-              </div>
-              <div className="w-full md:w-1/2 px-3">
-                <label className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2" htmlFor="grid-last-name">
-                  Last Name
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-last-name"
-                  type="text"
-                  placeholder="Last Name"
-                />
-              </div>
-            </div>
-  
-            <div className="w-full px-3 mb-6">
-              <label className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2" htmlFor="grid-email">
-                Email
+        {/* Right Column for Form */}
+        <div className="p-8 md:w-2/3">
+          <div className="flex flex-wrap mb-6 -mx-3">
+            <div className="w-full px-3 mb-6 md:w-1/2 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2"
+                htmlFor="grid-first-name"
+              >
+                First Name
               </label>
               <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-email"
-                type="email"
-                placeholder="inspirehub@example.com"
+                className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-first-name"
+                type="text"
+                placeholder="First Name"
               />
             </div>
-  
-            <div className="w-full px-3 mb-6">
-              <button
-                className="bg-[#5f7fbf] text-white font-bold py-2 px-4 w-full rounded hover:bg-[#3e60a3] transition-all duration-700"
+            <div className="w-full px-3 md:w-1/2">
+              <label
+                className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2"
+                htmlFor="grid-last-name"
+              >
+                Last Name
+              </label>
+              <input
+                className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+          </div>
+
+          <div className="w-full px-3 mb-6">
+            <label
+              className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2"
+              htmlFor="grid-email"
+            >
+              Email
+            </label>
+            <input
+              className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-email"
+              type="email"
+              placeholder="inspirehub@example.com"
+            />
+          </div>
+
+          <div className="flex flex-wrap mb-6 -mx-3">
+            <div className="w-full px-3 mb-6 md:w-1/2 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2"
+                htmlFor="grid-major"
+              >
+                Major
+              </label>
+              <input
+                className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-major"
+                type="text"
+                placeholder="Computer Science"
+              />
+            </div>
+            <div className="w-full px-3 md:w-1/2">
+              <label
+                className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2"
+                htmlFor="grid-academic-level"
+              >
+                Academic Level
+              </label>
+              <input
+                className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-academic-level"
+                type="text"
+                placeholder="Undergraduate"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center w-full gap-4 mt-10 mb-6">
+            <div className="px-2">
+              <a className="bg-[#5f7fbf] text-white font-bold py-2 px-14 rounded hover:bg-[#3e60a3] transition-all duration-700">
+                Submit
+              </a>
+            </div>
+
+            <div className="px-2">
+              <a
+                href="NewPasswordPage"
+                className="bg-[#5f7fbf] text-white font-bold py-2 px-4 rounded hover:bg-[#3e60a3] transition-all duration-700"
               >
                 Change Password
-              </button>
-            </div>
-  
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2" htmlFor="grid-major">
-                  Major
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-major"
-                  type="text"
-                  placeholder="Computer Science"
-                />
-              </div>
-              <div className="w-full md:w-1/2 px-3">
-                <label className="block uppercase tracking-wide text-[#bfa260] text-xs font-bold mb-2" htmlFor="grid-academic-level">
-                  Academic Level
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-academic-level"
-                  type="text"
-                  placeholder="Undergraduate"
-                />
-              </div>
-            </div>
-  
-            <div className="w-full px-3 mb-6">
-              <button
-                className="bg-[#5f7fbf] text-white font-bold py-2 px-4 w-full rounded hover:bg-[#3e60a3] transition-all duration-700"
-              >
-                Submit
-              </button>
+              </a>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
