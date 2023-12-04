753b625d761e40525cd5b44299fb271ee26ff9ef
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-04 11:15:32 +0300

    enhance: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify()
 });
